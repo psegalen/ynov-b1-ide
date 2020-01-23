8d6c008ec4cf547f9b32aa245f8a3719100eee47
Pierre Segalen <pierre.segalen@gmail.com>
2020-01-23 23:59:10 +0100

    Deuxième exercice - exercice prêt

---
+++ form/index.js
@@ -0,0 +1,13 @@
+var messagesErreur = [
+  "Votre nom ne peut être vide",
+  "Votre nom doit avoir une longueur d'au moins 3 caractères",
+  "Votre nom doit être complet (nom prénom), vous n'avez pas entré d'espace",
+  "Votre mot de passe ne peut être vide",
+  "Votre mot de passe doit faire au moins 6 caractères et contenir au moins une majuscule, une minuscule et un chiffre",
+  "Votre date de naissance doit être au format jj/mm/aaaa",
+  "Vous devez avoir 18 ans pour vous inscrire"
+];
+
+function inscrire() {
+  alert("Vérifier le contenu des champs");
+}
