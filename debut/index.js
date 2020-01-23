67ac4413391087d87f7f88acf7bb9f7cf2a38308
Pierre Segalen <pierre.segalen@gmail.com>
2020-01-23 21:49:54 +0100

    Start exercise 1

---
+++ debut/index.js
@@ -0,0 +1,11 @@
+function changerCouleur() {
+  alert("Il faut changer la couleur !!!");
+}
+
+function ajouterCarre() {
+  alert("Il faut ajouter un carré !!!");
+}
+
+function enleverCarre() {
+  alert("Il faut enlever un carré !!!");
+}
